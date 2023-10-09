49a356e5f7f8d7ddc35638fae6cb0aa9c18c0226
lijah <lijah@mansol.net.au>
2023-10-09 16:22:27 +0800

    Pick cell functionalities

--- src/Components/Tictactoe/Board.js
+++ src/Components/Tictactoe/Board.js
@@ -1,19 +1,43 @@
+import { useEffect, useState } from "react";
 import Square from "./Square";
 import "./Board.scss";
-import { Fragment } from "react";
+import ResultModal from "../Result/ResultModal";
 
 const Board = () => {
+  const [filledCount, setFilledCount] = useState(0);
+  const [combinationX, setCombinationX] = useState([]);
+  const [combinationO, setCombinationO] = useState([]);
+  const [isVisibleModal, setIsVisibleModal] = useState(false);
+
+  const playerHandler = (id, currentPlayer) => {
+    setFilledCount(filledCount + 1);
+    if (currentPlayer === 1) {
+      setCombinationX([...combinationX, id]);
+    } else {
+      setCombinationO([...combinationO, id]);
+    }
+  };
+
+  useEffect(() => {
+    console.log("filled count", filledCount);
+    if (filledCount === 9) {
+      setIsVisibleModal(true);
+    }
+  }, [filledCount]);
+
   return (
     <div className="board">
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
-      <Square />
+      <Square id={1} filledCount={filledCount} playerHandler={playerHandler} />
+      <Square id={2} filledCount={filledCount} playerHandler={playerHandler} />
+      <Square id={3} filledCount={filledCount} playerHandler={playerHandler} />
+      <Square id={4} filledCount={filledCount} playerHandler={playerHandler} />
+      <Square id={5} filledCount={filledCount} playerHandler={playerHandler} />
+      <Square id={6} filledCount={filledCount} playerHandler={playerHandler} />
+      <Square id={7} filledCount={filledCount} playerHandler={playerHandler} />
+      <Square id={8} filledCount={filledCount} playerHandler={playerHandler} />
+      <Square id={9} filledCount={filledCount} playerHandler={playerHandler} />
+
+      <ResultModal isVisibleModal={isVisibleModal} />
     </div>
   );
 };
--- src/Components/Tictactoe/Square.js
+++ src/Components/Tictactoe/Square.js
@@ -1,7 +1,37 @@
+import { useState, useEffect } from "react";
 import "./Square.scss";
+import CloseIcon from "@mui/icons-material/Close";
+import RadioButtonUncheckedIcon from "@mui/icons-material/RadioButtonUnchecked";
 
-const Square = () => {
-  return <div className="cell">Square</div>;
+const Square = (props) => {
+  const { id, filledCount, playerHandler } = props;
+  const [currentPlayer, setCurrentPlayer] = useState();
+
+  const handlePickCell = (id, filledCount) => {
+    if (filledCount % 2 === 0) {
+      setCurrentPlayer(1);
+    } else {
+      setCurrentPlayer(2);
+    }
+  };
+
+  useEffect(() => {
+    if (currentPlayer) {
+      playerHandler(id, currentPlayer);
+    }
+  }, [currentPlayer]);
+
+  return (
+    <div className="cell" onClick={(e) => handlePickCell(id, filledCount)}>
+      {currentPlayer === 1 ? (
+        <CloseIcon fontSize="large" />
+      ) : currentPlayer === 2 ? (
+        <RadioButtonUncheckedIcon fontSize="large" />
+      ) : (
+        ""
+      )}
+    </div>
+  );
 };
 
 export default Square;
